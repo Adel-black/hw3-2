91263522a43cb402b11d2f9c389a2939c0bd0b71
Adel-black <trifonova.alina@mail.ru>
2022-08-29 09:49:32 +1000

    code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,7 @@
 public class BmiService {
 
-    public int calculate(int imd) {
-        int mass = 60;
-        int high = 170;
-        return mass / high;
+    public int calculate(int mass, int high) {
+        int BMI = mass/high;
+        return BMI;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int mass = 60;
-        int high = 175;
-        int index = service.calculate(mass/high);
-        System.out.println(mass/high);
+        int BMI = service.calculate(65, 185);
+        System.out.println("Ваш индекс массы тела: ");
     }
 }
